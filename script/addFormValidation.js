77c7828b2ddf9b4683125a2de0096171f0f0e17e
Marco Preciado <mac46005@gmail.com>
2022-06-15 13:08:18 -0500

    add Validation form js is working correctly

--- script/addFormValidation.js
+++ script/addFormValidation.js
@@ -4,52 +4,57 @@ var isImpListFilled = false;
 
 var elAddBtn = document.querySelector("#addFormButton");
 
-function chkDescription(value){
-    isDescriptionFilled = isPatternMatch(/^[A-Za-z0-9]*$/, value);
+function chkDescription(el) {
+  if (el.value !== "") {
+    isDescriptionFilled = isPatternMatch(/^[A-Za-z0-9]*$/, el.value);
+  }
+
+  enableAddBtn();
 }
 
-function chkDate(){
-    var elDueDate = document.getElementById('dateDue');
-    var date = new Date(elDueDate.value);
-    console.log(date.toString());
-    if(date != null){
-        if(date > new Date()){
-            console.log('Selected Date is more than today');
-            isDueDateFilled = true;
-        }else{
-            console.log('Selected Date is less than today');
-            isDueDateFilled = false;
-        }
+function chkDate() {
+  var elDueDate = document.getElementById("dateDue");
+  var date = new Date(elDueDate.value);
+  console.log(date.toString());
+  if (date != null) {
+    if (date > new Date()) {
+      console.log("Selected Date is more than today");
+      isDueDateFilled = true;
+    } else {
+      console.log("Selected Date is less than today");
+      isDueDateFilled = false;
     }
+  }
+  enableAddBtn();
 }
-function chkImportance(){
-    var rdBtns = document.querySelectorAll('.rdBtn');
-    for (let i = 0; i < rdBtns.length; i++) {
-        if(rdBtns[i].checked == true){
-            isImpListFilled = true;
-            break;
-        }
+function chkImportance() {
+  var rdBtns = document.querySelectorAll(".rdBtn");
+  for (let i = 0; i < rdBtns.length; i++) {
+    if (rdBtns[i].checked == true) {
+      isImpListFilled = true;
+      break;
     }
-
+  }
+  enableAddBtn();
 }
 
-
-function isPatternMatch(pattern = RegExp, value ){
-    var reg = new RegExp(pattern);
-    return reg.test(value);
+function isPatternMatch(pattern = RegExp, value) {
+  var reg = new RegExp(pattern);
+  return reg.test(value);
 }
 
 // A function that enables the add button if the form is filled out correctly
-function enableAddBtn(){
-    chkDescription();
-    
-    if(isDescriptionFilled == true && isDueDateFilled == true && isImpListFilled == true){
-        elAddBtn.removeAttribute('disabled');
-        elAddBtn.classList.remove('disabled');
-    }
+function enableAddBtn() {
+  if (
+    isDescriptionFilled == true &&
+    isDueDateFilled == true &&
+    isImpListFilled == true
+  ) {
+    elAddBtn.removeAttribute("disabled");
+    elAddBtn.classList.remove("disabled");
+  } else {
+  }
 }
 // var isDescriptionFilled = true;
 // var isDueDateFilled = true;
 // var isImpListFilled = true;
-
-enableAddBtn();
