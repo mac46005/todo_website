83eeaa44ed264a0c64bb140242d292c763863631
Marco Preciado <mac46005@gmail.com>
2022-06-15 11:22:24 -0500

    date objects seem to be functional

--- script/addFormValidation.js
+++ script/addFormValidation.js
@@ -4,12 +4,27 @@ var isImpListFilled = false;
 
 var elAddBtn = document.querySelector("#addFormButton");
 
-// function chkDescription(value){
-//     value = value.trim();
-//     isDescriptionFilled = isPatternMatch(/^*$/, value);
-// }
+function chkDescription(value){
+    isDescriptionFilled = isPatternMatch(/^[A-Za-z0-9]*$/, value);
+}
 
-
+function chkDate(){
+    var elDueDate = document.getElementById('dateDue');
+    var date = new Date(elDueDate.value);
+    console.log(date.toString());
+    if(date != null){
+        if(date > new Date()){
+            console.log('Selected Date is more than today');
+        }else{
+            console.log('Selected Date is less than today');
+        }
+    }
+}
+function chkImportance(value){
+    var elImp1 = document.getElementById('importanceLevel1');
+    var elImp2 = document.getElementById('importanceLevel2');
+    var elImp3 = document.getElementById('importanceLevel3');
+}
 
 
 function isPatternMatch(pattern = RegExp, value ){
@@ -19,6 +34,8 @@ function isPatternMatch(pattern = RegExp, value ){
 
 // A function that enables the add button if the form is filled out correctly
 function enableAddBtn(){
+    chkDescription();
+    
     if(isDescriptionFilled == true && isDueDateFilled == true && isImpListFilled == true){
         elAddBtn.removeAttribute('disabled');
         elAddBtn.classList.remove('disabled');
