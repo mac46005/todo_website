5183c9edb1d993241771124ca1f9d7aa3cf12e86
Marco Preciado <mac46005@gmail.com>
2022-06-16 09:16:02 -0500

    found that isDue is being passed but is not being published as the correct data type

--- script/sqlConnection.php
+++ script/sqlConnection.php
@@ -5,7 +5,7 @@ $password = "";
 $dbName = "todolistdb";
 
 // Create connection
-$conn = new mysqli($servername,$username,$password);
+$conn = new mysqli($servername,$username,$password, $dbName);
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
