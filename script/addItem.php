d5ae264f009eb1264f49051224b0de1891cd5519
Marco Preciado <mac46005@gmail.com>
2022-06-16 09:29:16 -0500

    managed to add date to sql db table

--- script/addItem.php
+++ script/addItem.php
@@ -3,7 +3,7 @@ include('./sqlConnection.php');
 $description = $_REQUEST['description'];
 $dueDate = $_REQUEST['dateDue'];
 $impLvl = $_REQUEST['importanceLevel'];
-$sql = "INSERT INTO todo_items (description,duedate,importanceLevel) VALUES( '$description', $dueDate , '$impLvl')";
+$sql = "INSERT INTO todo_items (description,duedate,importanceLevel) VALUES( '$description', '$dueDate' , '$impLvl')";
 echo $sql;
 if($conn->query($sql) === TRUE){
     echo "New record created successfully";
